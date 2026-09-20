3690af2fc5494c72ebfb67a05bd7e26ec6cab37a
benjanmin huang <benjaminhuanghuang@gmail.com>
2026-09-19 17:33:08 -0700

    auth github

--- _notes/4-auth.md
+++ _notes/4-auth.md
@@ -12,8 +12,56 @@ npm i zod @hookform/resolvers react-hook-form
 npm i
 ```
 
+## Auth.js (next-auth)
+
 ```sh
 npm i next-auth@beta
 ```
 
-/auth.ts
+Create .env file
+
+```env
+AUTH_SECRET=
+AUTH_GITHUB_ID=
+AUTH_GITHUB_SECRET=
+
+```
+
+Create /middleware.ts
+Create /auth.ts
+
+Add Route Handler
+app/api/auth/[...nextauth]/route.ts
+
+## Setup GitHub Provider
+
+Github Settings -> Developer settings -> New OAth App
+
+Homepage URL: http://localhost:3000
+
+Authorization callback URL: http://localhost:3000/api/auth/callback/github
+
+Copy Client ID and Secret
+
+## Sign In
+
+```ts
+const handleSignIn = async (provider: "github" | "google") => {
+  try {
+    await signIn(provider, {
+      callbackUrl: ROUTES.HOME,
+      redirect: false,
+    });
+  } catch (error) {}
+};
+```
+
+## Session
+
+```tsx
+import { SessionProvider } from "next-auth/react";
+
+<SessionProvider session={session}>
+  <body></body>
+</SessionProvider>;
+```
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { ThemeProvider } from "next-themes";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+import { ReactNode } from "react";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -26,7 +29,9 @@ export const metadata: Metadata = {
   description: "Next.js fullstack App for question and answer",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
   // className actually applies the font; variable applies nothing, it just stores the font name in a CSS variable.
   return (
     <html
@@ -34,19 +39,23 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       suppressHydrationWarning
       className={cn("font-sans", geist.variable)}
     >
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          {children}
-        </ThemeProvider>
-        <Toaster richColors position="top-right" />
-      </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster richColors position="top-right" />
+        </body>
+      </SessionProvider>
     </html>
   );
-}
+};
+
+export default RootLayout;
